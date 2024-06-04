1f4b420b8ea1c669004655ffd6b59f1cfc3b3cec
Victor Madera <72475885+Klaz1k@users.noreply.github.com>
2024-06-04 19:43:54 -0400

    Añadido el buscado por tag y por id de leccion
    También se testeó el filtrado y paginación de la búsqueda de varios cursos

--- src/common/application/aspects/serviceLoggerDecorator.ts
+++ src/common/application/aspects/serviceLoggerDecorator.ts
@@ -1,3 +1,4 @@
+import { Result } from "src/common/domain/result-handler/result";
 import { IService, ServiceRequestDto, ServiceResponseDto } from "../interfaces/IService";
 import { IServiceDecorator } from "../interfaces/IServiceDecorator";
 import { IAsyncLogger } from "../ports/IAsyncLogger";
@@ -11,10 +12,12 @@ export class ServiceLoggerDecorator<I extends ServiceRequestDto, O extends Servi
     this.logger = logger;
   }
 
-  async execute(service: I): Promise<O> {
-    let r: O = await this.decoratee.execute(service);
+  async execute(service: I): Promise<Result<O>> {
+    let r = await this.decoratee.execute(service);
 
-    this.logger.writeToLog("Time: " + new Date() + " | Service: " + this.decoratee.constructor.name + " | InputData: "+ service.dataToString() + " | ResponseData: " + r.dataToString());
+    if (r.isSuccess) {
+      this.logger.writeToLog("Time: " + new Date() + " | Service: " + this.decoratee.constructor.name + " | InputData: "+ service.dataToString() + " | ResponseData: " + r.Value.dataToString());
+    }
 
     return r;
   }
--- src/course/application/services/getCourseById.service.ts
+++ src/course/application/services/getCourseById.service.ts
@@ -1,13 +1,20 @@
 import { Course } from "src/course/domain/Course";
 import { ICourseRepository } from "../repositories/ICourse.repository";
 import { IService, ServiceRequestDto, ServiceResponseDto } from "src/common/application/interfaces/IService";
+import { Result } from "src/common/domain/result-handler/result";
 
 export class GetCourseByIdService implements IService<GetCourseByIdRequest, GetCourseByIdResponse> {
   constructor(private readonly courseRepository: ICourseRepository){}
 
-  async execute(service: GetCourseByIdRequest): Promise<GetCourseByIdResponse> {
+  async execute(service: GetCourseByIdRequest): Promise<Result<GetCourseByIdResponse>> {
     const r = await this.courseRepository.getCourseById(service.courseId);
-    return new GetCourseByIdResponse(r);
+
+    if (r.isSuccess) {
+      return Result.success(new GetCourseByIdResponse(r.Value), r.StatusCode);
+    } else {
+      return Result.fail(r.Error, r.StatusCode, r.Message);
+    }
+    
   }
 }
 
