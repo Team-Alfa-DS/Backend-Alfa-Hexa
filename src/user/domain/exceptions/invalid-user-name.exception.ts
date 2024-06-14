dd938c3e35e3e66da1e4bbf6c470ac429dd6ab5b
Daniel Bortot <daniel.bortot@gmail.com>
2024-06-14 12:18:58 -0400

    refactor: :construction: creando ValueObjects

---
+++ src/user/domain/exceptions/invalid-user-name.exception.ts
@@ -0,0 +1,7 @@
+import { DomainException } from "src/common/domain/domain-exception";
+
+export class InvalidUserNameException extends DomainException {
+    constructor(msg: string) {
+        super(msg);
+    }
+}
