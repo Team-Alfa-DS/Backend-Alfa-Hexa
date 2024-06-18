0e52e22731f08cd5a31b16331c5f61a72e7fdf0b
Maria Paola Galvis <marugalvis04@gmail.com>
2024-06-17 22:34:56 -0400

    BlogTag VO creado

---
+++ src/blog/domain/valueObjects/blogTag.ts
@@ -0,0 +1,24 @@
+import { ValueObject } from "src/common/domain/value-object";
+
+export class BlogTag extends ValueObject<BlogTag>{
+    private constructor(public value: string) {
+        super();
+        if (!this.isValid(value)) {
+            throw new Error(`BlogTag ${value} is invalid`);
+        } 
+
+        this.value = value;
+    }
+
+    equals(blogTag: BlogTag): boolean {
+        return this.value === blogTag.value;
+    }
+
+    public static create(blogTag: string): BlogTag {
+        return new BlogTag(blogTag);
+    }
+
+    private isValid(value: string): boolean {
+        return value.length > 0 && value.length <= 20;
+    }
+}
