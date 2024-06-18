dc52221275ef488a876d29fc16fd93868de4cc11
Maria Paola Galvis <marugalvis04@gmail.com>
2024-06-17 22:32:53 -0400

    BlogId VO creado

---
+++ src/blog/domain/valueObjects/blogId.ts
@@ -0,0 +1,26 @@
+import { ValueObject } from "src/common/domain/value-object";
+
+export class BlogId extends ValueObject<BlogId> {
+    private constructor(public value: string) {
+        super();
+        if (!this.isValid(value)) {
+            throw new Error(`BlogId ${value} is invalid`);
+        } 
+
+        this.value = value;
+    }
+
+    equals(blogId: BlogId): boolean {
+        return this.value === blogId.value;
+    }
+
+    public static create(blogId: string): BlogId {
+        return new BlogId(blogId);
+    }
+
+    private isValid(value: string): boolean {
+        const UUID_FORMAT =/([0-9]|[a-f]){8,8}-([0-9]|[a-f]){4,4}-([0-9]|[a-f]){4,4}-([0-9]|[a-f]){4,4}-([0-9]|[a-f]){12,12}/g;
+        return !!value.match(UUID_FORMAT);
+
+    }
+}
