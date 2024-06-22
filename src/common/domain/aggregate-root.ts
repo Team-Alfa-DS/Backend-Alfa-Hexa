beb73fe5eedc90f1cd1c143bad0d3ccb897754d8
Daniel Bortot <daniel.bortot@gmail.com>
2024-06-21 20:26:29 -0400

    feat: :zap: manejo de eventos de dominio

--- src/common/domain/aggregate-root.ts
+++ src/common/domain/aggregate-root.ts
@@ -1,9 +1,28 @@
+import { DomainEvent } from "./domain-event";
 import { Entity } from "./entity";
 import { ValueObject } from "./value-object";
 
 export abstract class AggregateRoot<T extends ValueObject<T>> extends Entity<T> {
 
-    protected constructor(id: T) {
+    protected events: DomainEvent[] = [];
+
+    protected abstract when(event: DomainEvent): void;
+    protected abstract validateState(): void;
+
+    protected apply(event: DomainEvent): void {
+        this.when(event);
+        this.validateState();
+        this.events.push(event);
+    }
+
+    protected constructor(id: T, event: DomainEvent) {
         super(id);
+        this.apply(event);
+    }
+
+    pullDomainEvents(): DomainEvent[] {
+        const events = this.events;
+        this.events = [];
+        return events;
     }
 }
