f21247fd0cc9a04ae97973b48d72c5fcfedd0509
Daniel Bortot <daniel.bortot@gmail.com>
2024-06-28 19:08:24 -0400

    refactor: :construction: odm user mapper

--- src/user/infraestructure/mappers/odm-mappers/odm-user.mapper.ts
+++ src/user/infraestructure/mappers/odm-mappers/odm-user.mapper.ts
@@ -8,11 +8,22 @@ import { UserPassword } from "src/user/domain/value-objects/user-password";
 import { UserPhone } from "src/user/domain/value-objects/user-phone";
 import { UserType } from "src/user/domain/value-objects/user-type";
 import { UserImage } from "src/user/domain/value-objects/user-image";
+import { Model } from "mongoose";
+import { UserRole } from "src/user/domain/enums/role-user.type";
 
 export class OdmUserMapper implements IMapper<User, OdmUserEntity> {
 
     async toOrm(DomainEntity: User): Promise<OdmUserEntity> {
-        throw new Error("Method not implemented.");
+        const userOdm = new Model<OdmUserEntity>({
+            id: DomainEntity.Id.Id,
+            email: DomainEntity.Email.Email,
+            name: DomainEntity.Name.Name,
+            password: DomainEntity.Password.Password,
+            phone: DomainEntity.Phone.Phone,
+            type: DomainEntity.Type.Type as UserRole,
+            image: DomainEntity.Image?.Image
+        });
+        return userOdm;
     }
 
     async toDomain(odmEntity: OdmUserEntity): Promise<User> {
---
+++ src/user/infraestructure/repositories/odm-user.repository.ts
@@ -0,0 +1,47 @@
+import { Model } from "mongoose";
+import { Result } from "src/common/domain/result-handler/result";
+import { ITransactionHandler } from "src/common/domain/transaction-handler/transaction-handler.interface";
+import { IOdmUserRepository } from "src/user/application/repositories/odm-user-repository.interface";
+import { IUserRepository } from "src/user/domain/repositories/user-repository.interface";
+import { User } from "src/user/domain/user";
+import { UserEmail } from "src/user/domain/value-objects/user-email";
+import { UserId } from "src/user/domain/value-objects/user-id";
+import { OdmUserEntity } from "../entities/odm-entities/odm-user.entity";
+import { OdmUserMapper } from "../mappers/odm-mappers/odm-user.mapper";
+
+export class OdmUserRespository implements IOdmUserRepository {
+
+    private readonly userModel: Model<OdmUserEntity>;
+    private readonly odmUserMapper: OdmUserMapper;
+
+    constructor (userModel: Model<OdmUserEntity>, odmUserMapper: OdmUserMapper) {
+        this.userModel = userModel;
+        this.odmUserMapper = odmUserMapper;
+    }
+
+    async saveUser(user: User): Promise<void> {
+        const odmUser = await this.odmUserMapper.toOrm(user);
+        await this.userModel.create(odmUser);
+    }
+
+    async findUserById(id: UserId): Promise<Result<User>> {
+        try {
+            const user = await this.userModel.findOne({id: id.Id});
+            const domainUser = await this.odmUserMapper.toDomain(user);
+            return Result.success(domainUser, 200);
+        } catch(err) {
+            return Result.fail(new Error(err.message), err.code || 500, err.message || 'Ha ocurrido un error inesperado');
+        }
+    }
+
+    async findUserByEmail(email: UserEmail): Promise<Result<User>> {
+        try {
+            const user = await this.userModel.findOne({email: email.Email});
+            const domainUser = await this.odmUserMapper.toDomain(user);
+            return Result.success(domainUser, 200);
+        } catch(err) {
+            return Result.fail(new Error(err.message), err.code || 500, err.message || 'Ha ocurrido un error inesperado');
+        }
+    }
+
+}
