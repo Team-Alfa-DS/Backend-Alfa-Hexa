f345fb490ecbaa2dc214491db1fb1d66abbd8f09
Victor Madera <72475885+Klaz1k@users.noreply.github.com>
2024-06-29 17:32:11 -0400

    Se cambió la lógica de validación del trainer
    Se volteó la regex para la localización y nombre

--- src/trainer/domain/valueObjects/trainer-name.ts
+++ src/trainer/domain/valueObjects/trainer-name.ts
@@ -8,10 +8,10 @@ export class TrainerName extends ValueObject<TrainerName> {
     
     private constructor(name: string) {
         super();
-        const patron = /^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ]+$/;
+        const patron = /[`!¡@#$%^&*_+\-=\[\]{};':"\\|<>\/?¿~]/;
         
         if (!name ) throw new EmptyTrainerNameException( "El nombre del entrenador esta vacio" );
-        if (!patron.test(name)) throw new InvalidTrainerNameException( `El nombre del entrenador ${name} no puede 
+        if (patron.test(name)) throw new InvalidTrainerNameException( `El nombre del entrenador ${name} no puede 
             tener caracteres especiales` );
 
         this.name = Object.freeze(name); //*Esto funciona para que no pueda ser modificado
