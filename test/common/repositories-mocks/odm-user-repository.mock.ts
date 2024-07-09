acb0655094fb37a2613885f33697548821a5542a
Daniel Bortot <daniel.bortot@gmail.com>
2024-07-09 18:44:40 -0400

    test: :test_tube: terminando de agregar tests de user y auth

--- test/common/repositories-mocks/odm-user-repository.mock.ts
+++ test/common/repositories-mocks/odm-user-repository.mock.ts
@@ -1,5 +1,6 @@
 import { Result } from "src/common/domain/result-handler/result";
 import { IOdmUserRepository } from "src/user/application/repositories/odm-user-repository.interface";
+import { UserRole } from "src/user/domain/enums/role-user.type";
 import { User } from "src/user/domain/user";
 import { UserEmail } from "src/user/domain/value-objects/user-email";
 import { UserId } from "src/user/domain/value-objects/user-id";
@@ -8,20 +9,38 @@ import { UserName } from "src/user/domain/value-objects/user-name";
 import { UserPassword } from "src/user/domain/value-objects/user-password";
 import { UserPhone } from "src/user/domain/value-objects/user-phone";
 import { OdmUserEntity } from "src/user/infraestructure/entities/odm-entities/odm-user.entity";
+import { OdmUserMapper } from "src/user/infraestructure/mappers/odm-mappers/odm-user.mapper";
 
-export class OdmUserRepository implements IOdmUserRepository {
+export class OdmUserRepositoryMock implements IOdmUserRepository {
 
     private readonly users: User[] = [];
-    private readonly odmUsers: OdmUserEntity[] = [];
+    private readonly odmUsers: OdmUserEntity[] = [
+        {
+            id: '244fbecc-2127-433d-ba76-762fb2fee1c8', 
+            email: 'dbcd@gmail.com', 
+            name: 'Daniel Bortot',
+            password: '12345',
+            phone: '12345678910',
+            trainers: [],
+            type: UserRole.CLIENT,
+            image: null
+        }
+    ];
 
-    findUserById(id: UserId): Promise<Result<User>> {
-        throw new Error("Method not implemented.");
+    private readonly odmUserMapper = new OdmUserMapper();
+
+    async findUserById(id: UserId): Promise<Result<User>> {
+        const user = this.odmUsers.find(user => user.id === id.Id);
+        if (!user) return Result.fail(new Error('user not found'));
+        const userDomain = await this.odmUserMapper.toDomain(user);
+        return Result.success(userDomain);
     }
 
     async findUserByEmail(email: UserEmail): Promise<Result<User>> {
         const user = this.odmUsers.find(user => user.email === email.Email);
         if (!user) return Result.fail(new Error('user not found'));
-        return Result.success(user);
+        const userDomain = await this.odmUserMapper.toDomain(user);
+        return Result.success(userDomain);
     }
 
     async saveUser(user: User): Promise<void> {
---
+++ test/user/user-update.spec.ts
@@ -0,0 +1,66 @@
+import { BcryptEncryptor } from "src/auth/infraestructure/encryptor/bcrypt"
+import { UpdateUserRequest } from "src/user/application/dtos/request/update-user.request"
+import { UpdateUserService } from "src/user/application/services/update-user.application.service"
+import { InvalidUserEmailException } from "src/user/domain/exceptions/invalid-user-email.exception"
+import { InvalidUserNameException } from "src/user/domain/exceptions/invalid-user-name.exception"
+import { InvalidUserPhoneException } from "src/user/domain/exceptions/invalid-user-phone.exception"
+import { EventBusMock } from "test/common/event-manager/event-bus.mock"
+import { OdmUserRepositoryMock } from "test/common/repositories-mocks/odm-user-repository.mock"
+import { OrmUserRepositoryMock } from "test/common/repositories-mocks/orm-user-repository.mock"
+import { TransactionHandlerMock } from "test/common/transaction-handler-mock/transaction-handler.mock"
+
+describe('User Update', async () => {
+    it('debe fallar si no se envia un formato de nombre correcto a un usuario existente', async () => {
+        const request = new UpdateUserRequest('244fbecc-2127-433d-ba76-762fb2fee1c8', 'an', null, null, null, null);
+        const updateUserService = new UpdateUserService(
+            new OrmUserRepositoryMock(),
+            new OdmUserRepositoryMock(),
+            new TransactionHandlerMock(),
+            new BcryptEncryptor(),
+            new EventBusMock()
+        );
+        try {
+            await updateUserService.execute(request);
+        } catch (err) {
+            expect(err).toEqual(InvalidUserNameException);
+        }
+
+        expect.assertions(1);
+    })
+
+    it('debe fallar si no se envia un formato de email correcto a un usuario existente', async () => {
+        const request = new UpdateUserRequest('244fbecc-2127-433d-ba76-762fb2fee1c8', null, 'abcd@gmail', null, null, null);
+        const updateUserService = new UpdateUserService(
+            new OrmUserRepositoryMock(),
+            new OdmUserRepositoryMock(),
+            new TransactionHandlerMock(),
+            new BcryptEncryptor(),
+            new EventBusMock()
+        );
+        try {
+            updateUserService.execute(request);
+        } catch (err) {
+            expect(err).toEqual(InvalidUserEmailException);
+        }
+
+        expect.assertions(1);
+    })
+
+    it('debe fallar si no se envia un formato de telefono correcto a un usuario existente', async () => {
+        const request = new UpdateUserRequest('244fbecc-2127-433d-ba76-762fb2fee1c8', null, null, '12345', null, null);
+        const updateUserService = new UpdateUserService(
+            new OrmUserRepositoryMock(),
+            new OdmUserRepositoryMock(),
+            new TransactionHandlerMock(),
+            new BcryptEncryptor(),
+            new EventBusMock()
+        );
+        try {
+            updateUserService.execute(request);
+        } catch (err) {
+            expect(err).toEqual(InvalidUserPhoneException);
+        }
+
+        expect.assertions(1);
+    })
+})
