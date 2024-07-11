f71cb0969fd549589b1596bb28d25d79544c70e7
Maria Paola Galvis <marugalvis04@gmail.com>
2024-07-11 01:04:26 -0400

    Errores arreglados

--- src/blog/application/interfaces/getBlogsCountDTO.ts
+++ src/blog/application/interfaces/getBlogsCountDTO.ts
@@ -4,25 +4,13 @@ import { IsArray, IsNumber, IsOptional, IsString } from "class-validator";
 import { ServiceRequestDto } from "src/common/application/interfaces/IService";
 
 export class GetBlogsCountDTO implements ServiceRequestDto {
+  constructor(
+    public category?: string,
+    public  trainer?: string
+  ){
+
+  }
   dataToString(): string {
       return `category: ${this.category}, trainer: ${this.trainer}`;
   }
-
-  @ApiProperty({
-    description: 'Id categoria del blog',
-    example: '3fcfac6c-53d1-47f1-ae9a-2351a86c5d0d'
-  })
-  @IsOptional()
-  @IsString()
-  category?: string;
-
-
-  @ApiProperty({
-    description: 'Id del entrenador de los blogs a buscar',
-    example: '3fcfac6c-53d1-47f1-ae9a-2351a86c5d0d'
-  })
-  @IsOptional()
-  @IsString()
-  trainer?: string;
-
 }
--- src/blog/application/interfaces/getManyBlogsDTO.ts
+++ src/blog/application/interfaces/getManyBlogsDTO.ts
@@ -4,51 +4,16 @@ import { IsArray, IsNumber, IsOptional, IsString } from "class-validator";
 import { ServiceRequestDto } from "src/common/application/interfaces/IService";
 
 export class GetManyBlogsDTO implements ServiceRequestDto {
+  constructor(
+    public filter?: string,
+    public category?: string,
+    public trainer?: string,
+    public page?: number,
+    public perpage?: number
+  ){
+
+  }
   dataToString(): string {
       return `filter: ${this.filter}, category: ${this.category}, trainer: ${this.trainer}, page: ${this.page}, perpage: ${this.perpage}`;
   }
-  @ApiProperty({
-    description: 'filtros del blog',
-    example: '[Relajación]'
-  })
-  @IsOptional()
-  @IsString()
-  filter?: string;
-
-
-  @ApiProperty({
-    description: 'categoria del blog',
-    example: 'Yoga'
-  })
-  @IsOptional()
-  @IsString()
-  category?: string;
-
-
-  @ApiProperty({
-    description: 'Id del entrenador de los blogs a buscar',
-    example: '3fcfac6c-53d1-47f1-ae9a-2351a86c5d0d'
-  })
-  @IsOptional()
-  @IsString()
-  trainer?: string;
-
-
-  @ApiProperty({
-    description: 'Desde donde se empiezan a contar los blogs',
-    example: '1'
-  })
-  @IsOptional()
-  @IsNumber()
-  @Type(() => Number)
-  page?: number;
-  
-  @ApiProperty({
-    description: 'Cantidad de blogs que deseas recibir ',
-    example: '1'
-  })
-  @IsNumber()
-  @IsOptional()
-  @Type(() => Number)
-  perpage?: number;
 }
